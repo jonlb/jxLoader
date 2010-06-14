7d28b131d0c7045dfa4fefacb98051d67cc67e25
Jon Bomgardner <jonlb@comcast.net>
2010-06-14 12:10:19 -0700

    updates to bring things current

---
+++ example/repos/app/Source/test.js
@@ -0,0 +1,15 @@
+/*
+---
+
+name: Test
+
+description: This class is used to create managers to control stores, widgets, or other components
+
+license: MIT-style license.
+
+css
+
+provides: [test.js]
+
+...
+ */
